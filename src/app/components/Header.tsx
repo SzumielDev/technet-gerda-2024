8ebef8e5f3b984ab36d6da4bcb3acb60afdcdbe2
SzumielDev <107126687+SzumielDev@users.noreply.github.com>
2024-03-22 14:40:19 +0100

    style: set background image and header text

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,9 +1,20 @@
 import { Flex } from "@/app/components/layout/Flex";
+import header from "@/app/assets/header.jpg";
+import Image from "next/image";
 
 export const Header = () => {
     return (
-        <Flex className="h-screen flex items-center justify-center">
-            AAA
+        <Flex className={`h-screen flex items-center justify-center`}>
+            <div className="-z-10">
+                <Image
+                    src={header}
+                    layout="fill"
+                    objectFit="cover"
+                    quality={100}
+                    alt="header image"
+                />
+            </div>
+            <h1 className="text-4xl">JESTEŚMY DOSTĘPNI DLA CIEBIE</h1>
         </Flex>
     )
 }
