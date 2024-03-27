61853680c9a77573665bd93ae13dae4492998078
SzumielDev <107126687+SzumielDev@users.noreply.github.com>
2024-03-27 23:46:15 +0100

    style: header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -8,9 +8,13 @@ export const Header = () => {
     <main>
       <video className="video" src={require("/videos/video.mp4")} muted autoPlay loop controls={false} />
       <div className="absolute top-0 left-0 right-0">
-        <Flex className={`${roboto.className} h-screen flex items-center justify-center gap-x-4`}>
-          <p className="text-4xl text-white">TECHNET GERDA</p>
-          <button className="border px-4 py-2 rounded">ZOBACZ OFERTE</button>
+        <Flex className={`${roboto.className} h-screen items-center justify-center`}>
+          <Flex className="flex-col">
+            <Flex className="gap-x-4">
+              <p className="text-4xl text-white">TECHNET GERDA</p>
+              <button className="border px-4 py-2 rounded">ZOBACZ OFERTE</button>
+            </Flex>
+          </Flex>
         </Flex>
       </div>
     </main>
